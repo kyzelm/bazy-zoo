b9203f907a27bc21fbedd4281e821724d5e2c2cd
Damian Osiński <osinskidamian03@gmail.com>
2025-01-25 11:36:00 +0100

    Count

--- app/wybiegi/page.tsx
+++ app/wybiegi/page.tsx
@@ -1,12 +1,31 @@
-"use client";
-
 import {JSX} from "react";
+import {OracleDatabase} from "@/utils/oracleDatabase";
 
-export default function Infrastruktura(): JSX.Element {
+export default async function Wybiegi(): Promise<JSX.Element> {
+  const wybiegi = await OracleDatabase.query("SELECT ID_WYBIEG, NAZWA, (SELECT COUNT(*) FROM ZWIERZE Z WHERE W.ID_WYBIEG = Z.ID_WYBIEG GROUP BY Z.ID_WYBIEG) FROM WYBIEG W");
+
   return (
     <main>
       <h2>Wybiegi</h2>
       <br/>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nazwa</th>
+            <th>Liczba zwierząt</th>
+          </tr>
+        </thead>
+        <tbody>
+          {wybiegi.map((wybieg, index) => (
+            <tr key={index}>
+              <td>{wybieg[0]}</td>
+              <td>{wybieg[1]}</td>
+              <td>{wybieg[2] ? wybieg[2] : 0}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
